3bd0f97e82e45a1c8853670d56598f12ec01182d
alessio-cyber <Alessiocialdella.ac@gmail.com>
2025-02-11 20:03:48 +0100

    Add files via upload

---
+++ compile.bat
@@ -0,0 +1,9 @@
+@REM Parametro 1 = file.py da compilare
+pyinstaller.exe .\%1 --clean --onefile --windowed --noconsole -i"Calculator_31111.ico"
+rd /s /q build
+set str=%1
+set str=%str:~0,-3%
+del %str%.spec
+mkdir .\bin\
+copy .\dist\%str%.exe .\bin\ /B
+rd /s /q dist
